7f0a58f6a10e9decc8aa7dd44df7566b8de04f6a
Vlad-maker <skateislife@mail.ru>
2020-10-21 17:22:43 +0300

    refactore: JS comments + БЭМ структура

--- js/script.js
+++ js/script.js
@@ -1,29 +1,66 @@
 const editBtn = document.querySelector('.profile__info-button');
 const closeBtn = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-const nameInput = document.querySelector('input[name="name"]');
-const jobInput = document.querySelector('input[name="description"]');
-const nameContent = document.querySelector('.profile__info-name');
-const jobContent = document.querySelector('.profile__info-job');
+const profileName = document.querySelector('.profile__info-name');
+const profileJob = document.querySelector('.profile__info-job');
+let nameInput = document.querySelector('input[name="name"]');
+let jobInput = document.querySelector('input[name="description"]');
 let formElement = document.querySelector('.popup__content');
 
+//Открытие попапа
 function openPopup() {
-    nameInput.value = nameContent.innerText;
-    jobInput.value = jobContent.innerText;
+//Cкопировали первоначальное имя и должность в попап
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popup.classList.add('popup_opened');
 }
 editBtn.addEventListener('click', openPopup);
-
+//3акрытие попапа
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 closeBtn.addEventListener('click', closePopup);
 
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    nameContent.textContent = nameInput.value;
-    jobContent.textContent = jobInput.value;
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     closePopup();
 }
+formElement.addEventListener('submit', formSubmitHandler);
 
-formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
+
+
+
+
+
+
+
+// Находим форму в DOM
+//let formElement = // Воспользуйтесь методом querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+//function formSubmitHandler (evt) {
+  //  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
+
+    // Находим поля формы в DOM
+    //let nameInput = // Воспользуйтесь инструментом .querySelector()
+    //let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+    // Получите значение полей из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+//}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+//formElement.addEventListener('submit', formSubmitHandler); 
